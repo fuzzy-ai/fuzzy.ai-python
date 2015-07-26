a8ca04764513407f8a61b4982d6636c2851f12e3
Evan Prodromou <evan@fuzzy.io>
2015-07-26 15:50:37 -0700

    Configure version correctly

--- setup.py
+++ setup.py
@@ -1,22 +1,20 @@
 from codecs import open as codecs_open
 from setuptools import setup, find_packages
 
-
 # Get the long description from the relevant file
 with codecs_open('README.rst', encoding='utf-8') as f:
     long_description = f.read()
 
-
 setup(name='fuzzyio',
       version='0.0.1',
-      description=u"Skeleton of a Python package",
+      description=u"fuzzy.io API",
       long_description=long_description,
       classifiers=[],
       keywords='',
-      author=u"Sean Gillies",
-      author_email='sean@mapbox.com',
-      url='https://github.com/mapbox/fuzzyio',
-      license='MIT',
+      author=u"Evan Prodromou",
+      author_email='evan@fuzzy.io',
+      url='https://github.com/fuzzy-io/python',
+      license='Apache 2.0',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
