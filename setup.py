b73247164f0900fe9cdac903759ff3cf4e0bc40e
Evan Prodromou <evan@fuzzy.io>
2015-08-07 16:37:37 -0700

    0.2.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with codecs_open('README.rst', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='fuzzyio',
-      version='0.1.2',
+      version='0.2.0',
       description=u"fuzzy.io API",
       long_description=long_description,
       classifiers=[],
